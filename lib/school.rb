83230264b3cc73afd6d0047460db1f7dc068b3b2
kiran ram <kram@lions.molloy.edu>
2020-03-02 01:09:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,28 @@
 class School
-	attr_accessor :roster, :name, :grade 
-	attr_reader :school 
-	
-	def initialize(school)
-    @school = school
-  end
-  
-  def roster
-  	roster = {}
-  end 
-  
-  def add_student(name,grade)
-  	roster[grade] = []
-  	roster[grade] << name
-  		
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
 
-end
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
