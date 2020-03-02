f061e686789151971c3c953a61b65f7f6d4617cf
kiran ram <kram@lions.molloy.edu>
2020-03-02 01:04:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,19 @@
 class School
-  
-  def initialize(name)
-    @name = name
-    
-    roster = {}
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
   end
   
-  def add_student(student)
-    roster[""]
+  def roster
+  	roster = {}
+  end 
   
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
+
 end
